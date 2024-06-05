136b37745e16de995bbbeb8253555c6a25cb045c
VladTca <votsats@gmail.com>
2024-06-05 02:03:53 +0200

    🧪 Add a failing test.

--- src/03-tests/03.ts
+++ src/03-tests/03.ts
@@ -1,4 +1,5 @@
 import {student, StudentType} from "../02-test/02";
+import {CityType, governmentBuildingsType, HouseType} from "../02-test/02_02";
 
 export const sum = (a:number,b:number)=>{
     return a+b
@@ -26,4 +27,26 @@ export function makeStudentActive(s:StudentType) {
 
 export function doesStudentLiveIn(student: StudentType, cityName: string) {
     return student.address.city.title === cityName
+}
+export function addMoneyToBudget(building: governmentBuildingsType, budget: number){
+
+    building.budget+=budget
+
+}
+export function repairHouse(houseType: HouseType) {
+    houseType.repaired = true
+}
+
+export function toFireStaff(bilding: governmentBuildingsType, staffCountToFire: number){
+
+    bilding.staffCount-=staffCountToFire
+}
+
+export function toHireStaff(bilding: governmentBuildingsType, staffCountToHire: number){
+    bilding.staffCount+=staffCountToHire
+}
+
+export function createMessage(city: CityType){
+
+    return `Hello ${city.title} citizens. I want you be happy. All ${city.citizensNumber} man.`
 }
--- src/03-tests/03_02.test.ts
+++ src/03-tests/03_02.test.ts
@@ -1,4 +1,6 @@
-import {CityType} from "./02_02";
+import {CityType} from "../02-test/02_02";
+import {addMoneyToBudget, createMessage, repairHouse, toFireStaff, toHireStaff} from "./03";
+
 
 let city: CityType;
 
@@ -77,8 +79,44 @@ test('test city should contains 3 houses', () => {
 
 
 })
-test.skip('test city should contains 3 houses', () => {
-    expect(city.houses.length).toBe(3);
+test('Budget should be changed for Hospital', () => {
+    addMoneyToBudget(city.governmentBuildings[0], 100000);
+
+    expect(city.governmentBuildings[0].budget).toBe(300000);
+
+})
+
+test('Budget should be changed for Fire station', () => {
+    addMoneyToBudget(city.governmentBuildings[1], -100000);
+
+    expect(city.governmentBuildings[1].budget).toBe(400000);
+})
+
+test('Houses should be repaired', () => {
+
+    repairHouse(city.houses[1]);
+    expect(city.houses[1].repaired).toBeTruthy()  //true
+})
+
+test('staff should be decreased', () => {
+    toFireStaff(city.governmentBuildings[0], 20);
+
+    expect(city.governmentBuildings[0].staffCount).toBe(180)
+})
+
+test('staff should be increased', () => {
+    toHireStaff(city.governmentBuildings[0], 50);
+    toHireStaff(city.governmentBuildings[1], 50);
+
+    expect(city.governmentBuildings[0].staffCount).toBe(250)
+    expect(city.governmentBuildings[1].staffCount).toBe(1050)
+})
+
+
+test('Greetings should be correct for city', () => {
+
+ expect(createMessage(city)).toBe('Hello New York citizens. I want you be happy. All 1000000 man.')
+
 })
 
 test('test city should contains hospital and fire station', () => {
