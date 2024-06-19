7467258226a6cf837f4504fd4b2aa0d6d3b37bf7
VladTca <votsats@gmail.com>
2024-06-19 23:46:46 +0200

    🎨 Improve structure / format of the code.

---
+++ src/08-test/08_mutacii.test.ts
@@ -0,0 +1,24 @@
+
+type UserType = {
+    name: string
+    age: number
+}
+
+function increseAge(user: UserType) {
+    user.age = user.age + 1
+}
+
+
+
+test('big test', () => {
+   var user = {
+    name: 'Dimych',
+    age: 32
+  }
+})
+
+
+
+users['1'].name='Ekaterina'
+
+expect(users['1']).toEqual({id:1, name:'Ekaterina'})
